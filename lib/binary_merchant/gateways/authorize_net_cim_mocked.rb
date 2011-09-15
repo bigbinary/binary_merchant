58e9fb24f6c63630a56f00e2e583cc6edf1dffa2
Neeraj Singh <neerajdotname@gmail.com>
2011-09-15 16:09:29 -0400

    make xml more readable

--- lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
+++ lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
@@ -1,12 +1,39 @@
 module MockedCustomerProfileResponseXml
   def ssl_post(endpoint, data, headers = {})
-    %Q{<?xml version="1.0" encoding="utf-8"?><createCustomerProfileResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd"><messages><resultCode>Ok</resultCode><message><code>I00001</code><text>Successful.</text></message></messages><customerProfileId>4581836</customerProfileId><customerPaymentProfileIdList /><customerShippingAddressIdList /><validationDirectResponseList /></createCustomerProfileResponse>}
+    %Q{
+    <?xml version="1.0" encoding="utf-8"?>
+      <createCustomerProfileResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd">
+        <messages>
+          <resultCode>Ok</resultCode>
+          <message>
+            <code>I00001</code>
+            <text>Successful.</text>
+          </message>
+        </messages>
+        <customerProfileId>4581836</customerProfileId>
+        <customerPaymentProfileIdList />
+        <customerShippingAddressIdList />
+        <validationDirectResponseList />
+        </createCustomerProfileResponse>
+    }.strip.gsub(/\s\s+/, ' ').gsub(/>\s+/, '>').gsub(/\s+</,'')
   end
 end
 
 module MockedCustomerPaymentProfileResponseXml
   def ssl_post(endpoint, data, headers = {})
-    %Q{<?xml version="1.0" encoding="utf-8"?><createCustomerPaymentProfileResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd"><messages><resultCode>Ok</resultCode><message><code>I00001</code><text>Successful.</text></message></messages><customerPaymentProfileId>3981842</customerPaymentProfileId></createCustomerPaymentProfileResponse>}
+    %Q{
+    <?xml version="1.0" encoding="utf-8"?>
+      <createCustomerPaymentProfileResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd">
+        <messages>
+          <resultCode>Ok</resultCode>
+          <message>
+            <code>I00001</code>
+            <text>Successful.</text>
+          </message>
+        </messages>
+        <customerPaymentProfileId>3981842</customerPaymentProfileId>
+        </createCustomerPaymentProfileResponse>
+    }.strip.gsub(/\s\s+/, ' ').gsub(/>\s+/, '>').gsub(/\s+</,'')
   end
 end
 
