aa574f10d559f3b347d0516d522718fd841ac9c6
Neeraj Singh <neerajdotname@gmail.com>
2011-09-15 16:38:10 -0400

    full roundtrip test for authorization

--- lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
+++ lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
@@ -37,6 +37,24 @@ module MockedCustomerPaymentProfileResponseXml
   end
 end
 
+module MockedCustomerProfileTransactionResponseXml
+  def ssl_post(endpoint, data, headers = {})
+    %Q{
+    <?xml version="1.0" encoding="utf-8"?>
+      <createCustomerProfileTransactionResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd">
+        <messages>
+          <resultCode>Ok</resultCode>
+          <message>
+            <code>I00001</code>
+            <text>Successful.</text>
+          </message>
+        </messages>
+        <directResponse>1,1,1,This transaction has been approved.,PA4F54,Y,2162576907,,,200.00,CC,auth_only,,,,,,,,,,,,johny.walker@example.com,,,,,,,,,,,,,,21455FC1A0CA3A414774E8BE35E841AA,,2,,,,,,,,,,,XXXX0027,Visa,,,,,,,,,,,,,,,,</directResponse>
+      </createCustomerProfileTransactionResponse>
+    }.strip.gsub(/\s\s+/, ' ').gsub(/>\s+/, '>').gsub(/\s+</,'')
+  end
+end
+
 module ActiveMerchant
   module Billing
     class AuthorizeNetCimMockedGateway < AuthorizeNetCimGateway
@@ -68,7 +86,12 @@ module ActiveMerchant
       end
 
       def create_customer_profile_transaction(options)
-        Response.new(true, SUCCESS_MESSAGE, {'direct_response' => {'transaction_id' => AUTHORIZATION_TRANSACTION_ID }} , {} )
+        if make_roundtrip
+          self.send(:extend, MockedCustomerProfileTransactionResponseXml)
+          super
+        else
+          Response.new(true, SUCCESS_MESSAGE, {'direct_response' => {'transaction_id' => AUTHORIZATION_TRANSACTION_ID }} , {} )
+        end
       end
 
     end
