f40d6e7d91af1834cb0c92a9215cf0476dc0edc3
Neeraj Singh <neerajdotname@gmail.com>
2011-09-15 18:03:32 -0400

    mocked support for capture

--- lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
+++ lib/binary_merchant/gateways/authorize_net_cim_mocked.rb
@@ -91,7 +91,23 @@ module MockedCustomerProfileTransactionResponseXmlForRefund
   end
 end
 
-
+module MockedCustomerProfileTransactionResponseXmlForCapture
+  def ssl_post(endpoint, data, headers = {})
+    %Q{
+    <?xml version="1.0" encoding="utf-8"?>
+      <createCustomerProfileTransactionResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd">
+        <messages>
+          <resultCode>Ok</resultCode>
+          <message>
+            <code>I00001</code>
+            <text>Successful.</text>
+          </message>
+        </messages>
+        <directResponse>1,1,1,This transaction has been approved.,PA4F54,Y,2166026907,,,200.00,CC,auth_only,,,,,,,,,,,,johny.walker@example.com,,,,,,,,,,,,,,21455FC1A0CA3A414774E8BE35E841AA,,2,,,,,,,,,,,XXXX0027,Visa,,,,,,,,,,,,,,,,</directResponse>
+      </createCustomerProfileTransactionResponse>
+    }.strip.gsub(/\s\s+/, ' ').gsub(/>\s+/, '>').gsub(/\s+</,'')
+  end
+end
 
 module ActiveMerchant
   module Billing
@@ -153,6 +169,17 @@ module ActiveMerchant
             else
               Response.new(true, SUCCESS_MESSAGE, {'direct_response' => {'transaction_id' => REFUND_TRANSACTION_ID }} , {} )
             end
+
+          when :prior_auth_capture
+            if make_roundtrip
+              self.send(:extend, MockedCustomerProfileTransactionResponseXmlForCapture)
+              super
+            else
+              Response.new(true, SUCCESS_MESSAGE, {'direct_response' => {'transaction_id' => CAPTURE_TRANSACTION_ID }} , {} )
+            end
+
+
+
 
           end
         end
