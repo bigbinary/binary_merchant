3dbd9ab16fa0fb66ad4d6a0f10b1196333dcf59e
Neeraj Singh <neerajdotname@gmail.com>
2011-09-19 22:05:42 -0400

    refactor code to use autoload

--- lib/binary_merchant.rb
+++ lib/binary_merchant.rb
@@ -1,8 +1,12 @@
 require "binary_merchant/version"
-require "binary_merchant/gateway"
 
-require "binary_merchant/gateways/authorize_net"
+module BinaryMerchant
+  extend ActiveSupport::Autoload
+
+  autoload :Gateway
+  autoload :AuthorizeNetGateway, 'binary_merchant/gateways/authorize_net'
+  autoload :AuthorizeNetCimGateway, 'binary_merchant/gateways/authorize_net_cim'
+end
+
 require "binary_merchant/gateways/authorize_net_mocked"
-
-require "binary_merchant/gateways/authorize_net_cim"
 require "binary_merchant/gateways/authorize_net_cim_mocked"
