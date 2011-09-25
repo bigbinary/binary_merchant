334d7f385c25d9082fbdd16e7569f9f6a0032880
Neeraj Singh <neerajdotname@gmail.com>
2011-09-24 21:51:50 -0400

    raise exception only if value is nil

--- lib/binary_merchant/requires.rb
+++ lib/binary_merchant/requires.rb
@@ -6,12 +6,12 @@ module ActiveMerchant #:nodoc:
           raise ArgumentError.new("Missing required parameter: #{param.first}") unless hash.has_key?(param.first)
 
           # line added by binary_merchant
-          raise ArgumentError.new("Missing value for parameter: #{param.first}") unless hash[param.first]
+          raise ArgumentError.new("Missing value for parameter: #{param.first}") if hash[param.first].nil?
 
           valid_options = param[1..-1]
           raise ArgumentError.new("Parameter: #{param.first} must be one of #{valid_options.to_sentence(:words_connector => 'or')}") unless valid_options.include?(hash[param.first])
         else
-          raise ArgumentError.new("Missing required parameter: #{param}") unless hash.has_key?(param)
+          raise ArgumentError.new("Missing required parameter: #{param}") if hash.has_key?(param).nil?
 
           # line added by binary_merchant
           raise ArgumentError.new("Missing value for parameter: #{param}") unless hash[param]
