46df199111e31981ecc28be67d9cdf2dd60a77c8
Neeraj Singh <neerajdotname@gmail.com>
2011-10-01 23:49:12 -0400

    support for capture and void for cybersource

--- lib/binary_merchant/gateways/cyber_source.rb
+++ lib/binary_merchant/gateways/cyber_source.rb
@@ -6,10 +6,6 @@ module BinaryMerchant #:nodoc:
       super
     end
 
-    # Creates authorization for the given amount.
-    #
-    # === Options
-    #
     def authorize(options)
       response = gateway.authorize(options.fetch(:amount),
                                    options.fetch(:creditcard),
@@ -20,7 +16,20 @@ module BinaryMerchant #:nodoc:
       [transaction_id, response]
     end
 
+    def capture(options)
+      response = gateway.authorize(options.fetch(:amount),
+                                   options.fetch(:creditcard),
+                                   {order_id: options.fetch(:order_id),
+                                    email: options.fetch(:email),
+                                    billing_address: options.fetch(:address)})
+      transaction_id = response.success? ? response.params['requestID'] : nil
+      [transaction_id, response]
+    end
+
     def void(options)
+      response = gateway.void(options.fetch(:transaction_id))
+      transaction_id = response.success? ? response.params['requestID'] : nil
+      [transaction_id, response]
     end
 
   end
