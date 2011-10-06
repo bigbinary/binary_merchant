c37c043e14936d5a00d78254eef46a5cbb39d478
Neeraj Singh <neerajdotname@gmail.com>
2011-10-06 14:22:55 -0400

    support for reversal for cyber_source

--- lib/binary_merchant/gateways/cyber_source.rb
+++ lib/binary_merchant/gateways/cyber_source.rb
@@ -29,6 +29,17 @@ module BinaryMerchant #:nodoc:
       [transaction_id, response]
     end
 
+    def refund(options)
+      identification = []
+      identification << options.fetch(:order_id)
+      identification << options.fetch(:transaction_gid)
+      identification << options.fetch(:request_token)
+
+      response = gateway.refund(options.fetch(:amount),identification.join(';'))
+      transaction_id = response.success? ? response.params['requestID'] : nil
+      [transaction_id, response]
+    end
+
     def auth_reversal(options)
       identification = []
       identification << options.fetch(:order_id)
