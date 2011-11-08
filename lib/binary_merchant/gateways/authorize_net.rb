21c5a0c0c8756b9d73d7386dd506d57918cd2443
Neeraj Singh <neerajdotname@gmail.com>
2011-11-07 20:33:05 -0500

    more methods with better name

--- lib/binary_merchant/gateways/authorize_net.rb
+++ lib/binary_merchant/gateways/authorize_net.rb
@@ -6,11 +6,19 @@ module BinaryMerchant
       super
     end
 
-    def get_transaction_id_from_authorize(response)
+    def get_transaction_id_for_purchase(response)
       response.success? ? response.params['transaction_id'] : nil
     end
 
-    def get_transaction_id_from_void(response)
+    def get_transaction_id_for_authorize(response)
+      response.success? ? response.params['transaction_id'] : nil
+    end
+
+    def get_transaction_id_for_void(response)
+      response.success? ? response.params['transaction_id'] : nil
+    end
+
+    def get_transaction_id_for_capture(response)
       response.success? ? response.params['transaction_id'] : nil
     end
 
